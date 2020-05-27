9eef46f5dfe9d66516bf941665f5f2f14fcd77c0
zahariaalexandra <62277649+zahariaalexandra@users.noreply.github.com>
2020-05-27 23:23:55 +0300

    Create Extensions.cs

---
+++ MVP-tema3-OnlineRestaurant/MVP-tema3-OnlineRestaurant/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MVP_tema3_OnlineRestaurant
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> GetChildrenOfType<T>(this DependencyObject start) where T : class
+        {
+            var queue = new Queue<DependencyObject>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var item = queue.Dequeue();
+
+                var realItem = item as T;
+                if (realItem != null)
+                {
+                    yield return realItem;
+                }
+
+                int count = VisualTreeHelper.GetChildrenCount(item);
+                for (int i = 0; i < count; i++)
+                {
+                    queue.Enqueue(VisualTreeHelper.GetChild(item, i));
+                }
+            }
+        }
+    }
+}
